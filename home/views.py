19843ea3832e6caf4162453d5fe557b109ab0cc5
shahramsss <shahramsss1987@gmail.com>
2025-08-02 11:01:54 +0330

    add home

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, APIView
 
-# Create your views here.
+
+# @api_view(["GET", "POST", "PUT"])
+# def home(request):
+#     return Response({"name": "sss"})
+
+
+class Home(APIView):
+    def get(self, request):
+        return Response({"name": "s1"})
