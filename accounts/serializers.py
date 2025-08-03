3896c7e5098e3864056ace49b9003584fc03e9bb
shahramsss <shahramsss1987@gmail.com>
2025-08-03 11:16:52 +0330

    add modelseriailizer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 def clean_email(value):
@@ -6,19 +7,39 @@ def clean_email(value):
         raise serializers.ValidationError("admin cant be in email!")
 
 
-class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True, validators=[clean_email])
-    password = serializers.CharField(write_only=True, required=True)
+# class UserRegisterSerializer(serializers.Serializer):
+#     username = serializers.CharField(required=True)
+#     email = serializers.EmailField(required=True, validators=[clean_email,])
+#     password = serializers.CharField(write_only=True, required=True)
+#     password2 = serializers.CharField(write_only=True, required=True)
+
+#     def validate_username(self, value):
+#         if value == "admin":
+#             raise serializers.ValidationError("user name cant be `admin`")
+#         return value
+
+#     def validate(self, data):
+#         if data["password"] != data["password2"]:
+#             raise serializers.ValidationError("passwords must match!")
+#         return data
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(write_only=True, required=True)
 
-    def validate_username(self, value):
-        if value == "admin":
-            raise serializers.ValidationError("user name cant be `admin`")
-        return value
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "password2")
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "email": {
+                "validators": [
+                    clean_email,
+                ]
+            },
+        }
 
     def validate(self, data):
         if data["password"] != data["password2"]:
             raise serializers.ValidationError("passwords must match!")
-
         return data
